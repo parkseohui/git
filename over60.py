de23150c4865c098a00deb5a63fbaf43bdb9a482
parkseohui <skfhddl003@gmail.com>
2019-01-24 18:36:57 +0900

    range

--- over60.py
+++ over60.py
@@ -1,6 +1,5 @@
 #60점 이상 통과
 marks=[90,25,67,45,80]
-
 number=0
 for mark in marks:
     number=number+1
@@ -8,9 +7,9 @@ for mark in marks:
         print("%d번 학생을 합격입니다."%number)
     else:
         print("%d번 학생은 불합격입니다."%number)
+
         
 #continue
-
 number=0
 for mark in marks:
     number=number+1
@@ -18,3 +17,12 @@ for mark in marks:
         print("%d번 학생은 합격입니다."%number)
     else:
         continue
+
+
+#range
+for number in range(len(marks)):
+    if marks[number]<60:
+        continue
+    print("%d번 학생 축하합니다. 합격"%(number+1))
+    
+          
