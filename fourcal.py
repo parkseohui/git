70c5182859a66072a45188ca4507fef3dcc6856d
skfhddl003 <skfhddl003@gmail.com>
2019-01-26 22:24:40 +0900

    fourcal

---
+++ fourcal.py
@@ -0,0 +1,10 @@
+class fourcal:
+    def setdata(self,first,second):
+        self.first=first
+        self.second=second
+
+    def add(self):
+        result=self.first+self.second
+        return result
+
+
