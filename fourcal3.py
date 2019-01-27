38cdbe03d74568594e85f8ed1916b949c3e4bc0a
skfhddl003 <skfhddl003@gmail.com>
2019-01-27 19:51:47 +0900

    class init

---
+++ fourcal3.py
@@ -0,0 +1,26 @@
+class fourcal3:
+    def __init__(self,first,second):
+        self.first=first
+        self.second=second
+
+    def setdata(self,first,second):
+        self.first=first
+        self.second=second
+
+    def add(self):
+        result=self.first+self.second
+        return result
+
+    def mul(self):
+        result=self.first*self.second
+        return result
+
+    def sub(self):
+        result=self.first-self.second
+        return result
+
+    def div(self):
+        result=self.first/self.second
+        return result
+
+    
