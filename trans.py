cd899b50bc16eccd7527b7b09e74c78184204003
parkseohui <skfhddl003@gmail.com>
2019-02-06 23:54:08 +0900

    수정

--- trans.py
+++ trans.py
@@ -1,4 +1,5 @@
 #숫자를입력받으면 그에맞는자릿수 출력
+'''
 num=input("숫자를 입력해주세요")
 result=[1]
 for i in num:
@@ -6,3 +7,12 @@ for i in num:
 result.pop()
 
 print(str(result),"의 자리수입니다.")
+'''
+num=input("숫자를입력해주세요.")
+result=1
+
+for i in range(len(num)-1):
+    result=10*result
+
+
+print(result)
