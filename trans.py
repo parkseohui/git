6464639654f71b7387129a19c62bd42ea7158d61
parkseohui <skfhddl003@gmail.com>
2019-02-07 00:19:11 +0900

    최모씨의 반란

--- trans.py
+++ trans.py
@@ -8,6 +8,7 @@ result.pop()
 
 print(str(result),"의 자리수입니다.")
 '''
+'''
 num=input("숫자를입력해주세요.")
 result=1
 
@@ -16,3 +17,14 @@ for i in range(len(num)-1):
 
 
 print(result)
+
+'''
+
+num=input("숫자를입력해주세요.")
+result=[(10**i)*10 for i in range(len(num)-1)]
+result.reverse()
+for i in range(len(result)-1):
+    result.pop()
+print(str(result)+'의 자리')
+
+#컴프리핸션을 좋아하는 최모씨의 부탁으로 만들어봤습니다.
